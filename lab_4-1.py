5491e2d5914587333dd5dd9711d6a2707d9c3fe2
p22dhealy <p22dhealy@fairfieldprep.org>
2021-10-26 10:12:15 -0400

    submit lab 4

---
+++ lab_4-1.py
@@ -0,0 +1,14 @@
+# author: DMH 10/26/21
+
+print("Fairfield" + " Prep")
+
+print('-' * 20)
+
+print("I'm loving this short story I'm reading, \"The Fall of the House of Usher\"")
+
+print(len("I'm loving this short story I'm reading, \"The Fall of the House of Usher\""))
+
+string = 'apple'
+
+print(string[3])
+
