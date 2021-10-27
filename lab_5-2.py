02c5f43524e0660687ea6b259d4fa0eb4e97ceee
p22dhealy <p22dhealy@fairfieldprep.org>
2021-10-27 10:14:06 -0400

    submit lab 5

---
+++ lab_5-2.py
@@ -0,0 +1,12 @@
+#author: DMH 10/27/21
+
+user1 = input("Please enter the first string:")
+
+user2 = input("Please enter the second string:")
+
+if user2 > user1:
+    print("The first string is less than the second string")
+if user2 < user1:
+    print("The first string is larger than the second string")
+if user1 == user2:
+    print("The strings are equal")
