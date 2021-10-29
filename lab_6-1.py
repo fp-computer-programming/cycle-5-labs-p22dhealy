5c5542e271bc57a3e1ce9fc321113551027dd05a
p22dhealy <p22dhealy@fairfieldprep.org>
2021-10-29 10:44:30 -0400

    submit lab 6

---
+++ lab_6-1.py
@@ -0,0 +1,9 @@
+#author: DMH 10/29/21
+
+string = 'flibbertigibbet'
+
+print(string.find('t'))
+
+x = 'I wish, I wish, I was a fish'
+
+print(x.split(','))
---
+++ lab_6-2.py
@@ -0,0 +1,7 @@
+# author: DMH 10.29.21
+
+person1 = input("What is your name?")
+
+person2 = input("What is your name?")
+
+print("Hello, {0}. My name is {1}.".format(person1, person2))
